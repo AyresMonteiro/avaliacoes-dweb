8e5fa2c68706151e663ebe151cea8e93f0e0badb
Ayres Monteiro <ayresmonteiro52@gmail.com>
2020-11-18 08:58:41 -0200

    2020-12-01 exercicio 003

---
+++ exercicios-2020-12-01/exec003.php
@@ -0,0 +1,14 @@
+<!-- Crie uma lista que contenha o nome das 5 músicas que você mais gosta. Ao
+final, imprima a segunda e quarta música. -->
+
+<?php
+  list($m1, $m2, $m3, $m4, $m5) = array(
+    "Vê se não morre - Supercombo",
+    "Hyori Ittai - Yuzu",
+    "A Busca - Codinome Winchester",
+    "Tarde Livre - Selvagens à Procura de Lei",
+    "Hit the road Jack! - Ray Charles"
+  );
+
+  echo "$m2<br>$m4<br>";
+?>
